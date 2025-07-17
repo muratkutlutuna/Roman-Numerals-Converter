566f8aedb1178cb21f507e98639921cd38100b5a
muratkutlutuna <muratkutlutuna@gmail.com>
2025-07-17 10:27:45 +0200

    open_port_80

--- app.py
+++ app.py
@@ -52,5 +52,5 @@ def index():
                                developer_name='Kutlu')
 
 if __name__ == '__main__':
-    app.run(debug=True)
-    # app.run(host='0.0.0.0', port=80)
+    # app.run(debug=True)
+    app.run(host='0.0.0.0', port=80)
